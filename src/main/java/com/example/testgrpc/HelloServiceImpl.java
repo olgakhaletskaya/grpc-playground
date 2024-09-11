d6eebc9548fc10b47a5d1254688c1ddde5642de0
Olha Khaletska <olhakhaletska@Olhas-Air.attlocal.net>
2024-09-11 16:43:47 -0700

    initial commit

---
+++ src/main/java/com/example/testgrpc/HelloServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.testgrpc;
+
+
+import com.example.testgrpc.proto.HelloReply;
+import com.example.testgrpc.proto.HelloRequest;
+import com.example.testgrpc.proto.Profile;
+import com.example.testgrpc.proto.SimpleGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.concurrent.TimeUnit;
+
+@GrpcService
+public class HelloServiceImpl extends SimpleGrpc.SimpleImplBase {
+
+//    mvn spring-boot:run
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        String greeting = "Hello, " + request.getName() + "!";
+        HelloReply response = HelloReply.newBuilder().setMessage(greeting).build();
+
+        // Send the response back to the client
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void register(Profile request, StreamObserver<Profile> responseObserver) {
+        String greeting = "Hello, " + request.getName() + "!";
+        HelloReply response = HelloReply.newBuilder().setMessage(greeting).build();
+
+        // Send the response back to the client
+        try {
+            responseObserver.onNext(request);
+            TimeUnit.SECONDS.sleep(1);
+            responseObserver.onNext(request);
+            TimeUnit.SECONDS.sleep(1);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onCompleted();
+        }
+    }
+}
---
+++ src/main/java/com/example/testgrpc/TestGrpcApplication.java
@@ -0,0 +1,15 @@
+package com.example.testgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGrpcApplication.class, args);
+	}
+
+
+
+}
